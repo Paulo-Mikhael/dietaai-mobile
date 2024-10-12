3f7b2ed9de47eea6aa0808dc28345f833f455e4d
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-10-12 13:41:05 -0400

    Tela inicial

---
+++ app/index.tsx
@@ -0,0 +1,54 @@
+import { Text, View, Image, StyleSheet, Pressable } from "react-native";
+import { colors } from "@/constants/colors";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/images/logo.png")} />
+      <Text style={styles.title}>
+        Dieta<Text style={{ color: colors.white }}>.AI</Text>
+      </Text>
+      <Text style={styles.text}>Sua dieta personalizada com inteligência</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Gerar dieta</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
+    gap: 8,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: colors.green,
+  },
+  text: {
+    fontSize: 16,
+    color: colors.white,
+    maxWidth: 240,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: colors.blue,
+    width: "100%",
+    height: 40,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 34,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
---
+++ constants/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  background: "#1a4b4b",
+  white: "#FFF",
+  green: "#1EB500",
+  orange: "#F79600",
+  blue: "#009AD6",
+  black: "#000",
+};
